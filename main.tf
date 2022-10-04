e5a41767b9a6caec5481c5486dbf8629ce4ac667
Maria Hetmańska <hetmanskamaria@gmail.com>
2022-10-05 00:32:29 +0200

    add credentials

--- main.tf
+++ main.tf
@@ -2,8 +2,12 @@
 # PROVIDERS
 ##################################################################################
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 provider "aws" {
-  profile = "awsprofile"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region     = var.aws_region
 }
 
