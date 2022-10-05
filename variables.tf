d96ff3d5791b313b507779314101c1cefcc494b8
Maria Hetmańska <hetmanskamaria@gmail.com>
2022-10-05 20:56:29 +0200

    fix

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key" {
-  type        = string
-  description = "AWS Access Key"
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS Secret Key"
-  sensitive   = true
-}
-
 variable "aws_region" {
   type        = string
   description = "Region for AWS Resources"
