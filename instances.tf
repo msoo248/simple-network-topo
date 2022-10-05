d6062b7b9840c4c7ff1788317cf42a098c8fb417
Maria Hetmańska <hetmanskamaria@gmail.com>
2022-10-05 20:51:57 +0200

    fix

--- instances.tf
+++ instances.tf
@@ -2,17 +2,28 @@
 # DATA
 ##################################################################################
 
-data "aws_ssm_parameter" "ami" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-}
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 ##################################################################################
 # INSTANCES 
 ##################################################################################
 resource "aws_instance" "quagga" {
   count                  = var.instance_count
-  ami                    = 06148e0e81e5187c8
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
