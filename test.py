50fff2f7e1b3635069481c496674b47a4f7f9366
glamarye <104938783+glamarye@users.noreply.github.com>
2023-01-29 22:26:27 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,34 +3,49 @@ import os
 import subprocess
 
 @pytest.mark.parametrize('ip', [
-    '172.31.2.74',
-    '172.31.69.200',
-    '10.0.0.1',
-    '10.0.0.2'
+    '10.0.1.10',
+    '10.0.1.20',
+    '10.0.2.10',
+    '10.0.2.20',
+    '10.0.3.10',
+    '10.0.3.20',
+    '10.0.4.10',
+    '10.0.4.20'
 ])
 
 def test_ping(ip):
     output = os.system('ping -c 1 ' + ip)
     assert output == 0, f'Ping to {ip} failed'
 
+
+
 def test_neighbor_exist():
     output = subprocess.run(['sudo', 'vtysh', '-c', 'show ip ospf neighbor'], capture_output = True, text = True)
     assert output.returncode == 0
     assert 'No neighbors found' not in output.stdout
 
-def test_correct_neighbor():
+@pytest.mark.parametrize('neighbor_ip', [
+    '10.0.0.1',
+    '10.0.0.6'
+])
+
+def test_correct_neighbor(neighbor_ip):
     output = subprocess.run(['sudo', 'vtysh', '-c', 'show ip ospf neighbor'], capture_output = True, text = True)
-    searched_ip = '10.0.0.2'
     found = False
     for line in output.stdout.split('\n'):
         if not line:
             continue
         ip_addr = line.split()[0]
-        if ip_addr == searched_ip:
+        if ip_addr == neighbor_ip:
                 found = True
                 break
-    assert found, f'Neighbor with address {searched_ip} does not exist'
-def test_ospf():
-    output = subprocess.run(['sudo', 'vtysh', '-c', 'show ip ospf neighbor'], capture_output = True, test = True)
+    assert found, f'Neighbor with address {neighbor_ip} does not exist'
+    
+@pytest.mark.parametrize('ospf_id', [
+    '10.0.0.2',
+    '10.0.0.5'
+])
+def test_ospf(ospf_id):
+    output = subprocess.run(['sudo', 'vtysh', '-c', 'show ip ospf'], capture_output = True, test = True)
     ospf_conf = output.stdout
-    assert "Router ID: 10.0.0.1" in ospf_conf, f'OSPF with this ID does not exist'
+    assert f"Router ID: {ospf_id}" in ospf_conf, f'OSPF with this ID does not exist'
